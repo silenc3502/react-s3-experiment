800c97ea8dd0947f14343dc5e7220f8c665975a7
Sanghoon Lee <gcccompil3r@gmail.com>
2025-08-06 09:49:39 +0900

    chore: remove useless log

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import S3Manager from "./components/S3Manager";
 
-console.log("ENV REGION:", process.env.REACT_APP_S3_REGION);
-console.log("ENV BUCKET:", process.env.REACT_APP_S3_BUCKET);
-
 const App = () => (
     <div className="p-6">
         <h1 className="text-2xl font-bold mb-4">🧺 S3 파일 관리</h1>
